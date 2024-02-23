700d6a84025be2db7f963b32fce85cf8efcf3eff
Shobhit Agarwal <shobhitagarwal2003@gmail.com>
2024-02-23 20:20:38 +0530

    Completed basic sequelize setup for create a db

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express=require("express");
-require('dotenv').config()
+// require('dotenv').config()
+// The above lines makes the dotenv package load our environment variables from .dotenv file into process.env object
+// process is a node object.
+const bodyParser=require("body-parser");
+
 const {PORT}=require("./config/ServerConfig");
 
 const setupAndStartServer=async()=>{
     // create the express object
     const app=express();
    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`);
         // console.log(process.env);
